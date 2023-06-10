319ff64182c165da807d60545daf2a88a56256aa
Meshack Korir <meshackkorir100@gmail.com>
2023-06-10 11:37:36 +0300

    showed

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,9 @@
 class Show < ActiveRecord::Base
-  
+    has_many :characters
+    belongs_to :network
+
+    def actors_list
+        actors = characters.map(&:actor)
+        actors.map(&:full_name)
+    end
 end
